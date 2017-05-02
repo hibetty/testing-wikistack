653c7df038c401d191ff9815ec8f40428c29a6d4
Betty Lin <bettyhappylin@gmail.com>
2017-05-02 15:17:20 -0400

    findByTag

--- test/page.model.spec.js
+++ test/page.model.spec.js
@@ -28,6 +28,7 @@ describe('Page model', function () {
   });
 
   describe('Class methods', function () {
+
     before(function (done) {
       Page.create({
         title: 'foo',
@@ -40,15 +41,57 @@ describe('Page model', function () {
       .catch(done);
     });
 
+    afterEach(function(done){
+      Page.destroy({
+        where: {},
+        truncate: true
+      }).then(function(){
+        done();
+      }).catch(done);
+    });
+
     describe('findByTag', function () {
-      it('gets pages with the search tag', function () {
-        var theseTags = findByTag(function())
-      }
-      it('does not get pages without the search tag');
+      it('gets pages with the search tag', function(done) {
+        Page.findByTag('foo')
+        .then(function(pages){
+          expect(pages).to.have.lengthOf(1);
+          done();
+        }).catch(done);
+      });
+      it('does not get pages without the search tag', function(done){
+        Page.findByTag('hello')
+        .then(function(pages){
+          expect(pages).to.have.lengthOf(0);
+          done();
+        }).catch(done);
+      });
     });
   });
 
   describe('Instance methods', function () {
+    // var page1 = Page.create({
+    //     title: 'One',
+    //     content: 'bar',
+    //     tags: ['foo', 'bar']
+    //   });
+
+    //   var page2 = Page.create({
+    //     title: 'Two',
+    //     content: 'lalala',
+    //     tags: ['foo']
+    //   });
+
+    //   var page3 = Page.create({
+    //     title: 'Three',
+    //     content: 'hi',
+    //     tags: ['hello', 'world']
+    //   });
+
+
+    //   before(function(){
+    //     return Promise.all([page1, page2, page3]);
+    //   });
+
     describe('findSimilar', function () {
       it('never gets itself');
       it('gets other pages with any common tags');
